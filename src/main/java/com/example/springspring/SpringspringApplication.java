4acadd890059558b6eaf676e50a2a4b134594eef
hun1i <nicebrian0328@gmail.com>
2024-03-13 19:17:13 +0900

    ADD :: 초기설정

---
+++ src/main/java/com/example/springspring/SpringspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.springspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringspringApplication.class, args);
+	}
+
+}
