6f4500d33bf445128e1cfa632a449ee6337abaf6
hun1i <nicebrian0328@gmail.com>
2024-03-20 16:58:28 +0900

    FIX :: Transaction 라이브러리 잘못씀..

--- src/main/java/com/example/springstudyiscold/domain/auth/service/AuthService.java
+++ src/main/java/com/example/springstudyiscold/domain/auth/service/AuthService.java
@@ -4,12 +4,10 @@ import com.example.springstudyiscold.domain.auth.presentation.dto.CreateUserDto;
 import com.example.springstudyiscold.domain.auth.presentation.dto.LoginUserDto;
 import com.example.springstudyiscold.domain.user.domain.User;
 import com.example.springstudyiscold.domain.user.repository.UserRepository;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -25,10 +23,10 @@ public class AuthService {
         };
         throw new DuplicateKeyException("유저 존재");
     }
-
+    @Transactional(readOnly = true)
     public User login(LoginUserDto loginUserDto) {
-        User findUser = userRepository.findByUserId(loginUserDto.userId());
+        User findUser = userRepository.findByUserId(loginUserDto.userId()); // 나중에 bcrypt 적용하고 existByUserId로 고치자
         // bcrypt 부분(만약에 비밀번호가 맞다면)
-        return findUser; // 지금은 JWT는 너무 일러용.. security 설정이랑 bcrypt 먼저 해보고 할게용.
+        return findUser;
     }
 }
